65083eec76b3f7a56c37280b3dbdd84b44cc7702
Emrekaratas01 <158616191+Emrekaratas01@users.noreply.github.com>
2025-07-31 13:13:31 -0300

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Open this page at [https://emrekaratas01.github.io/zar/](https://emrekaratas01.github.io/zar/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/emrekaratas01/zar** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/emrekaratas01/zar** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,59 @@
+let ZAR = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showString("MERHABA!")
+})
+input.onGesture(Gesture.Shake, function () {
+    ZAR = randint(1, 6)
+    if (ZAR == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (ZAR == 2) {
+        basic.showLeds(`
+            . . . # #
+            . . . # #
+            . . . . .
+            # # . . .
+            # # . . .
+            `)
+    } else if (ZAR == 3) {
+        basic.showLeds(`
+            . . . # #
+            . . # # #
+            . # # # .
+            # # # . .
+            # # . . .
+            `)
+    } else if (ZAR == 4) {
+        basic.showLeds(`
+            # # . # #
+            # # . # #
+            . . . . .
+            # # . # #
+            # # . # #
+            `)
+    } else if (ZAR == 5) {
+        basic.showLeds(`
+            # # . # #
+            # # # # #
+            . # # # .
+            # # # # #
+            # # . # #
+            `)
+    } else if (ZAR == 6) {
+        basic.showLeds(`
+            # # . # #
+            # # . # #
+            # # . # #
+            # # . # #
+            # # . # #
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
